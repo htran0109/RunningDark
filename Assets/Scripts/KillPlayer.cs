9673e2a69f71bd4ad12244f2deee5c1f7ff7a237
Abi <abixcastro@gmail.com>
2017-01-21 14:52:27 -0800

    death

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class KillPlayer : MonoBehaviour {
 
 
-	void OnTriggerEnter2D(Collider2D other){
 
-		if(other.gameObject.tag == "Player")
 
-			Destroy(other.gameObject);
+    void OnTriggerEnter2D(Collider2D other){
+
+        if (other.gameObject.tag == "Player")
+
+            other.gameObject.GetComponent<DeathSequence>().Invoke("Die",0);
+
+        
 
 		}
 			}
