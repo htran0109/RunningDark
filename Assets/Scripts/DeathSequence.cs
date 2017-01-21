9dcf894fa4a3b81ac964ec373dc684b1c2bad8c6
Abi <abixcastro@gmail.comabixcastro@gmail.com>
2017-01-21 12:17:12 -0800

    Kill player death and sequence

---
+++ Assets/Scripts/DeathSequence.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSequence : MonoBehaviour {
+
+	public GameObject deathMovement;
+
+	AudioSource audio;
+	Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+
+		audio = GetComponent<AudioSource> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public GameObject Player;
+
+	public void Die() {
+
+
+
+
+
+}
