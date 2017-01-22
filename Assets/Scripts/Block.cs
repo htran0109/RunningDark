ae9eef85b55c7a5a4fdb975203f85290d140cd80
hqtran <hqtran@ucsd.edu>
2017-01-21 22:24:06 -0800

    Fixed hat hitbox

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    public float targetAlpha = 0;
+    public float startAlpha = 0;
+    public float startTime = 0;
+
+
+    bool fadeIn = false;
+
+    public float fadeInTime = 0.1f;
+    public float fadeOutTime = 2;
+    
+
     private float visibility = 0;
+    private float currAlpha = 0;
+    
     public bool isClimbable = false;
 	// Use this for initialization
 	void Start () {
@@ -13,24 +26,45 @@ public class Block : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		if(visibility < 0 )
+        float fadeInProgress = (Time.time - startTime) / fadeInTime;
+        float fadeOutProgress = (Time.time - startTime) / fadeOutTime;
+        if (fadeIn)
+        {
+            currAlpha = Mathf.Lerp(startAlpha, targetAlpha, fadeInProgress);
+            if(fadeInProgress >= 1)
+            {
+                fadeIn = false;
+                targetAlpha = 0;
+                startAlpha = currAlpha;
+                startTime = Time.time;
+            }   
+        }
+        else
+        {
+            currAlpha = Mathf.Lerp(startAlpha, targetAlpha, fadeOutProgress);
+           
+        }
+		/*if(visibility > 0 )
         {
             visibility -= 170 * Time.deltaTime;
             //Debug.Log(visibility);
-        }
+        }*/
         Color temp = gameObject.GetComponent<SpriteRenderer>().color;
-        temp.a = visibility/255;
+        temp.a = currAlpha;
         gameObject.GetComponent<SpriteRenderer>().color = temp;
     }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        //Debug.Log(coll.gameObject.name + "Hit" );
-        if(coll.gameObject.tag == "GroundPing")
-        {
+            //Debug.Log(coll.gameObject.name + "Hit" );
+            if(coll.gameObject.tag == "GroundPing")
+            {
             //Debug.Log("Illuminated");
-            visibility = 255;
-        }
+            //visibility = 255;
+            startAlpha = currAlpha;
+            targetAlpha = 1;
+            startTime = Time.time;
+            fadeIn = true;
+            }
     }
 }
