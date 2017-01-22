69479f3f6710671d005bf20d7124095af57ba1aa
Robert Wickline <robert.wickline3@gmail.com>
2017-01-21 22:11:01 -0800

    Enemy Update

---
+++ Assets/DestoryPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        if (otherObject.tag == "Player")
+        {
+            Destroy(otherObject.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
